14f27f6b270084be8ee5852e2fef97da1e6e515a
rayza-piedade24 <rayza.janice5@outlook.com>
2024-10-28 14:56:18 +0000

    commit modified

--- Assignment_1/plates.py
+++ Assignment_1/plates.py
@@ -1,12 +1,19 @@
 def main():
     plate= input("Plate Number:")
-    if is_valid(True):
+    if is_valid(plate):
         print("Valid")
     else:
         print("False")
         
-    def is_valid(s):
-        if s[:2].isalpha() and s.isalnum and 2 <len(s) <6:
-            return True
-        else:
+def is_valid(s):
+    if s.isalnum() and 1 < len(s) <7 and s[:2].isalpha() :
+        if s[2:].isdigit("0"):
             return False
+        elif s[2:].isdigit() and s.endswith().isalpha():
+            return False
+        else:
+            return True
+    else:
+        return False
+          
+main()
