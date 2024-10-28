583941cb40dd8c9ff31b5cb00518d1acf442990a
rayza-piedade24 <rayza.janice5@outlook.com>
2024-10-28 16:51:14 +0000

    commit modified

--- Assignment_1/plates.py
+++ Assignment_1/plates.py
@@ -7,24 +7,24 @@ def main():
         
 def is_valid(s):
     while True:
-        if s.isalnum() and 1 < len(s) <7 and s[:2].isalpha() :
+        if s.isalnum() and 1 < len(s) <7 and s[:1].isalpha() :
             if s[:].isalpha() :
                 return True
-            else:
+            elif s[:].isalnum() :
                 digits = 0 #Number of digits in license plate
                 for i in s:
                     if i.isdigit:
                         digits= digits + 1
-                if digits == 1 and s[-1].isdigit and s[-1] !=0 :
-                    return True
-                elif digits == 2 and s[-2:].isdigit and s[-2] !=0 :
-                    return True
-                elif digits == 3 and s[-3:].isdigit and s[-3] !=0 :
-                    return True
-                elif digits == 4 and s[-4:].isdigit and s[-4] !=0 :
-                    return True
-                else:
-                    return False
+                        if digits == 1 and s[-1].isdigit and s[-1] != '0' :
+                            return True
+                        elif digits == 2 and s[::-2].isdigit and s[-2] != '0' :
+                            return True
+                        elif digits == 3 and s[-3:].isdigit and s[-3] != '0' :
+                            return True
+                        elif digits == 4 and s[-4:].isdigit and s[-4] != '0' :
+                            return True
+                        else:
+                            return False
         else:
             return False
           
